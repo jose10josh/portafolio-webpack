078fc49816362cf267e15717be026ccebbf7d3ea
José Arévalo <joseaf1002@gmail.com>
2023-01-17 10:32:59 -0600

    Create env file and config for dev enviroment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -15,6 +12,7 @@ module.exports = {
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
+	mode: 'development',
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -83,13 +81,5 @@ module.exports = {
       ]
     }),
     new Dotenv(),
-    new CleanWebpackPlugin()
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin()
-    ]
-  }
 }
